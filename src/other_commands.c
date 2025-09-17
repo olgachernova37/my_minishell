2fa846d49ffe3aece52f374d372ce1dba6b5259b
olgachernova37 <32342614+olgachernova37@users.noreply.github.com>
2025-09-17 15:45:53 +0000

    other_commands_finished

--- src/other_commands.c
+++ src/other_commands.c
@@ -11,6 +11,20 @@
 /* ************************************************************************** */
 #include "../minishell.h"
 
+
+void free_split(char **split)
+{
+    int i = 0;
+    if (!split)
+        return;
+    while (split[i])
+    {
+        free(split[i]);
+        i++;
+    }
+    free(split);
+}
+
 char *get_path_from_env(t_env **env)
 {
     while (*env)
@@ -22,28 +36,96 @@ char *get_path_from_env(t_env **env)
     return (NULL);
 }
 
+char **input_with_null_terminator(char **input)
+{
+    int i = 0;
+    while (input[i])
+        i++;
+    char **new_input = malloc(sizeof(char *) * (i + 2));
+    i = 0;
+    while (input[i])
+    {
+        new_input[i] = ft_strdup(input[i]);
+        i++;
+    }
+    new_input[i] = NULL;
+    new_input[i + 1] = NULL;
+    return (new_input);
+}
 
 
-int other_commands_implementation(char **input, t_env **env)
+char *find_command_path(char **input, t_env **env)
 {
     char **splited_path;
     char *path_value = get_path_from_env(env);
-   // printf("PATH value: %s\n", path_value);
+    char *path_with_command;
+    char *input_command;
+    int i = 0;
 
     splited_path = ft_split(path_value, ':');
-    // while (*splited_path)
-    // {
-    //     printf("SPLITED PATH: %s\n", *splited_path);
-    //     splited_path++;
-    // }
-
     if (!*splited_path)
-        return (1);
-    int i = 0;
-    char *input_command = ft_strjoin(input[0], "/");
-    printf("INPUT COMMAND: %s\n", input_command);
-    // while (splited_path[i])
-    // {
-    //     char *path_with_command = ft_strjoin(splited_path[i], 
-    // }
+        return NULL;
+    input_command = ft_strjoin("/", input[0]);
+    while (splited_path[i])
+    {
+        path_with_command = ft_strjoin(splited_path[i], input_command);
+        if (access(path_with_command, X_OK) == 0)
+        {
+            // printf("FOUND COMMAND: %s\n", path_with_command);
+            return path_with_command;
+        }
+        i++;
+    }
+    free(input_command);
+    free_split(splited_path);
+    return NULL;
 }
+// Howwwww????????
+// void close_fd(void)
+// {
+//     int fd = 3;
+//     while (fd < MAX_FD) {
+//         close(fd);
+//         fd++;
+//     }
+// }
+
+
+
+
+int execute_command(char *path_with_command, char **input)
+{
+    char **new_input = input_with_null_terminator(input);
+    pid_t pid = fork();
+    if (pid == 0)
+    {
+        execve(path_with_command, new_input, NULL);
+        perror("execve");
+        exit(EXIT_FAILURE);
+    }
+    else if (pid > 0)
+    {
+        waitpid(pid, NULL, 0);
+    }
+    else
+    {
+        perror("fork");
+        return -1;
+    }
+    return 0;
+}
+
+int other_commands_implementation(char **input, t_env **env)
+{
+    char *path_with_command = find_command_path(input, env);
+    if (path_with_command)
+    {
+        return execute_command(path_with_command, input);
+    }
+    else
+    {
+        fprintf(stderr, "bash: %s: command not found\n", input[0]);
+        return 127;
+    }
+}
+
